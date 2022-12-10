a8e9c0058364d3355154fa5ddba7a7c4be53857a
ned <github@edoardo.fyi>
2022-12-10 13:29:04 +0100

    use sync telegram apis

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,10 @@
-import asyncio
 import logging
 
 import telegram.constants as constants
-from asyncChatGPT.asyncChatGPT import Chatbot as ChatGPT3Bot
+from revChatGPT.revChatGPT import Chatbot as ChatGPT3Bot
+
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 
 
 class ChatGPT3TelegramBot:
@@ -22,100 +22,81 @@ class ChatGPT3TelegramBot:
         self.disallowed_message = "Sorry, you are not allowed to use this bot. You can check out the source code at " \
                                   "https://github.com/n3d1117/chatgpt-telegram-bot"
 
-    async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    def help(self, update: Update, context: CallbackContext):
         """
         Shows the help menu.
         """
-        await update.message.reply_text("/start - Start the bot\n"
-                                        "/reset - Reset conversation\n"
-                                        "/help - Help menu\n\n"
-                                        "Open source at https://github.com/n3d1117/chatgpt-telegram-bot",
-                                        disable_web_page_preview=True)
+        update.message.reply_text("/start - Start the bot\n"
+                                  "/reset - Reset conversation\n"
+                                  "/help - Help menu\n\n"
+                                  "Open source at https://github.com/n3d1117/chatgpt-telegram-bot",
+                                  disable_web_page_preview=True)
 
-    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    def start(self, update: Update, context: CallbackContext):
         """
         Handles the /start command.
         """
         if not self.is_allowed(update):
             logging.info(f'User {update.message.from_user.name} is not allowed to start the bot')
-            await self.send_disallowed_message(update, context)
+            self.send_disallowed_message(update, context)
             return
 
         logging.info('Bot started')
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a Chat-GPT3 Bot, please talk to me!")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a Chat-GPT3 Bot, please talk to me!")
 
-    async def reset(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    def reset(self, update: Update, context: CallbackContext):
         """
         Resets the conversation.
         """
         if not self.is_allowed(update):
             logging.info(f'User {update.message.from_user.name} is not allowed to reset the bot')
-            await self.send_disallowed_message(update, context)
+            self.send_disallowed_message(update, context)
             return
 
         logging.info('Resetting the conversation...')
         self.gpt3_bot.reset_chat()
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
 
-    async def send_typing_periodically(self, update: Update, context: ContextTypes.DEFAULT_TYPE, every_seconds):
-        """
-        Sends the typing action periodically to the chat
-        """
-        while True:
-            await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=constants.ChatAction.TYPING)
-            await asyncio.sleep(every_seconds)
-
-    async def prompt(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    def prompt(self, update: Update, context: CallbackContext):
         """
         React to incoming messages and respond accordingly.
         """
         if not self.is_allowed(update):
             logging.info(f'User {update.message.from_user.name} is not allowed to use the bot')
-            await self.send_disallowed_message(update, context)
+            self.send_disallowed_message(update, context)
             return
 
         logging.info(f'New message received from user {update.message.from_user.name}')
 
-        # Send "Typing..." action periodically every 4 seconds until the response is received
-        typing_task = asyncio.get_event_loop().create_task(
-            self.send_typing_periodically(update, context, every_seconds=4)
-        )
-        response = await self.get_chatgpt_response(update.message.text)
-        typing_task.cancel()
-
-        await context.bot.send_message(
+        context.bot.send_chat_action(chat_id=update.effective_chat.id, action=constants.CHATACTION_TYPING)
+        response = self.get_chatgpt_response(update.message.text)
+        context.bot.send_message(
             chat_id=update.effective_chat.id,
             reply_to_message_id=update.message.message_id,
             text=response['message'],
-            parse_mode=constants.ParseMode.MARKDOWN
+            parse_mode=constants.PARSEMODE_MARKDOWN
         )
 
-    async def get_chatgpt_response(self, message) -> dict:
+    def get_chatgpt_response(self, message) -> dict:
         """
         Gets the response from the ChatGPT APIs.
         """
         try:
-            response = await self.gpt3_bot.get_chat_response(message)
+            response = self.gpt3_bot.get_chat_response(message)
             return response
         except Exception as e:
             logging.info(f'Error while getting the response: {str(e)}')
             return {"message": "I'm having some trouble talking to you, please try again later."}
 
-    async def send_disallowed_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    def send_disallowed_message(self, update: Update, context: CallbackContext):
         """
         Sends the disallowed message to the user.
         """
-        await context.bot.send_message(
+        context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=self.disallowed_message,
             disable_web_page_preview=True
         )
-
-    async def error_handler(self, update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
-        """
-        Handles errors in the telegram-python-bot library.
-        """
-        logging.debug(f'Exception while handling an update: {context.error}')
 
     def is_allowed(self, update: Update) -> bool:
         """
@@ -129,13 +110,13 @@ class ChatGPT3TelegramBot:
         """
         Runs the bot indefinitely until the user presses Ctrl+C
         """
-        application = ApplicationBuilder().token(self.config['token']).build()
+        updater = Updater(token=self.config['token'])
+        dispatcher = updater.dispatcher
 
-        application.add_handler(CommandHandler('start', self.start))
-        application.add_handler(CommandHandler('reset', self.reset))
-        application.add_handler(CommandHandler('help', self.help))
-        application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.prompt))
+        dispatcher.add_handler(CommandHandler('start', self.start))
+        dispatcher.add_handler(CommandHandler('reset', self.reset))
+        dispatcher.add_handler(CommandHandler('help', self.help))
+        dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), self.prompt))
 
-        application.add_error_handler(self.error_handler)
-
-        application.run_polling()
+        updater.start_polling()
+        updater.idle()
